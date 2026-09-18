0c18e114b467a4729ea6198cf3237a1aec83f554
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-09-18 16:21:20 +1000

    https://github.com/mP1/walkingkooka/pull/3326 PublicStaticHelperTesting extends PublicClassTesting
    - https://github.com/mP1/walkingkooka/pull/3326
    - PublicStaticHelperTesting extends PublicClassTesting

--- src/test/java/walkingkooka/tree/expression/convert/provider/TreeExpressionConvertProvidersTest.java
+++ src/test/java/walkingkooka/tree/expression/convert/provider/TreeExpressionConvertProvidersTest.java
@@ -30,11 +30,6 @@ public final class TreeExpressionConvertProvidersTest implements PublicStaticHel
     }
 
     @Override
-    public JavaVisibility typeVisibility() {
-        return JavaVisibility.PUBLIC;
-    }
-
-    @Override
     public boolean canHavePublicTypes(final Method method) {
         return false;
     }
